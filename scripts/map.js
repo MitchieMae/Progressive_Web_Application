a3d50f2dc3211520012648f9b60b3d1e21607653
gabozkhi <gabrielbucsit5@gmail.com>
2018-04-13 22:02:02 +0800

    added touch event handlers

--- scripts/map.js
+++ scripts/map.js
@@ -17,9 +17,9 @@ bottom_rightCoor.long = 120.632203;
 mapCanvas.height = document.body.clientHeight;
 mapCanvas.width = document.body.clientWidth;
 
-if(mapCanvas.height > mapCanvas.width){
-    mapCanvas.width =  mapCanvas.height;
-}else{
+if (mapCanvas.height > mapCanvas.width) {
+    mapCanvas.width = mapCanvas.height;
+} else {
     mapCanvas.height = mapCanvas.width;
 }
 
@@ -29,107 +29,135 @@ mapBg.width = mapCanvas.width;
 mapBg.height = mapCanvas.height;
 var testItem = document.getElementById("testItem");
 
-mapBg.onload = function(){
-    mapCtx.drawImage(mapBg,0,0);
+mapBg.onload = function () {
+    mapCtx.drawImage(mapBg, 0, 0);
 }
 
-var mapTranslation = [0,0];
-var mapScale = [1,1];
+var mapTranslation = [0, 0];
+var mapScale = [1, 1];
 
-mapCanvas.addEventListener("mousemove",moveMap);
-mapCanvas.addEventListener("mousedown",mouseDown);
-mapCanvas.addEventListener("mouseup",mouseUp);
-//TODO touch events
+mapCanvas.addEventListener("mousemove", moveMap);
+mapCanvas.addEventListener("mousedown", mouseDown);
+mapCanvas.addEventListener("mouseup", mouseUp);
+mapCanvas.addEventListener("touchstart", touchPress);
+mapCanvas.addEventListener("touchmove", touchMoveMap);
+mapCanvas.addEventListener("touchend", touchRelease);
 
 
 ///mapcontrols
 var zoomInBtn = document.getElementById("mapCtr_zoom-in");
 var zoomOutBtn = document.getElementById("mapCtr_zoom-out");
 
-zoomInBtn.addEventListener("click",zoomIn);
-zoomOutBtn.addEventListener("click",zoomOut);
+zoomInBtn.addEventListener("click", zoomIn);
+zoomOutBtn.addEventListener("click", zoomOut);
 
-var mapper_X = new RangeMap(top_leftCoor.long,bottom_rightCoor.long,
-                            0,mapBg.width);
-var mapper_Y = new RangeMap(top_leftCoor.lat,bottom_rightCoor.lat,
-                            0,mapBg.height);
+var mapper_X = new RangeMap(top_leftCoor.long, bottom_rightCoor.long,
+    0, mapBg.width);
+var mapper_Y = new RangeMap(top_leftCoor.lat, bottom_rightCoor.lat,
+    0, mapBg.height);
 
-function plotOnMap(elem,lat, long){
-    mapCtx.drawImage(elem,mapper_X.map(long),mapper_Y.map(lat));
+function plotOnMap(elem, lat, long) {
+    mapCtx.drawImage(elem, mapper_X.map(long), mapper_Y.map(lat));
 }
 
-function plotOnMap(elem,lat, long,width,height){
-    mapCtx.drawImage(elem,mapper_X.map(long),mapper_Y.map(lat),width,height);
+function plotOnMap(elem, lat, long, width, height) {
+    mapCtx.drawImage(elem, mapper_X.map(long), mapper_Y.map(lat), width, height);
 }
 
-var mousePressed = false;
+var mousePressed, touchPressed = false;
 
-function mouseDown(event){
+function mouseDown(event) {
     mousePressed = true;
     prevCursPos_X = event.clientX;
     prevCursPos_Y = event.clientY;
     event.stopPropagation();
 }
 
-function mouseUp(event){
+function mouseUp(event) {
     mousePressed = false;
     event.stopPropagation();
 }
 
-var prevCursPos_X, prevCursPos_Y = undefined;
-
-function moveMap(event){
+function touchPress(event) {
+    touchPressed = true;
+    prevTouchPos_X = event.touches[0].clientX;
+    prevTouchPos_Y = event.touches[0].clientY;
     event.stopPropagation();
-    if(!mousePressed) return;
-    if(prevCursPos_X === undefined && prevCursPos_Y === undefined){
+}
+
+function touchRelease(event) {
+    touchPressed = false;
+    event.stopPropagation();
+}
+
+var prevCursPos_X, prevCursPos_Y, prevTouchPos_X, prevTouchPos_Y = undefined;
+
+function moveMap(event) {
+    event.stopPropagation();
+    if (!mousePressed) return;
+    if (prevCursPos_X === undefined && prevCursPos_Y === undefined) {
         prevCursPos_X = event.clientX;
         prevCursPos_Y = event.clientY;
-    }else{
+    } else {
         offsetMap(event.clientX - prevCursPos_X,
-                  event.clientY - prevCursPos_Y);
+            event.clientY - prevCursPos_Y);
         prevCursPos_X = event.clientX;
         prevCursPos_Y = event.clientY;
     }
+    
 }
 
-function offsetMap(x_offset,y_offset){
+function touchMoveMap(event) {
+    if (!touchPressed) return;
+    if (prevTouchPos_X === undefined && prevTouchPos_Y === undefined) {
+        prevTouchPos_X = event.touches[0].clientX;
+        prevTouchPos_Y = event.touches[0].clientY;
+    } else {
+        offsetMap(event.touches[0].clientX - prevTouchPos_X,
+                  event.touches[0].clientY - prevTouchPos_Y);
+        prevTouchPos_X = event.touches[0].clientX;
+        prevTouchPos_Y = event.touches[0].clientY;
+    }
+}
+
+function offsetMap(x_offset, y_offset) {
     mapTranslation[0] += x_offset;
     mapTranslation[1] += y_offset;
     draw();
 }
 
-function zoomIn(){
+function zoomIn() {
     mapScale[0] += .5;
     mapScale[1] += .5;
     draw();
 }
 
-function zoomOut(){
+function zoomOut() {
     mapScale[0] -= .5;
     mapScale[1] -= .5;
     draw();
 }
 
-function draw(){
-    mapCtx.clearRect(0,0,mapCanvas.width,mapCanvas.height)
+function draw() {
+    mapCtx.clearRect(0, 0, mapCanvas.width, mapCanvas.height)
     mapCtx.save();
-    mapCtx.translate(mapTranslation[0],mapTranslation[1]);
-    mapCtx.scale(mapScale[0],mapScale[1]);
-    mapCtx.drawImage(mapBg,0,0);
+    mapCtx.translate(mapTranslation[0], mapTranslation[1]);
+    mapCtx.scale(mapScale[0], mapScale[1]);
+    mapCtx.drawImage(mapBg, 0, 0);
     drawPolice();
     mapCtx.restore();
 }
 
-function drawPolice(){
-    for(i = 0; i < stations.length; i++){
-        if(stations[i].ServiceType = "Police Department"){
+function drawPolice() {
+    for (i = 0; i < stations.length; i++) {
+        if (stations[i].ServiceType = "Police Department") {
             var icon = new Image();
             icon.height = 10;
             icon.width = 10;
             icon.src = "../assets/icons/police.png"
             var lat = Number(stations[i].Coordinates.split(',')[0]);
             var lon = Number(stations[i].Coordinates.split(',')[1]);
-            plotOnMap(icon,lat,lon,50 * 1/mapScale[0],50 * 1/mapScale[1]);
+            plotOnMap(icon, lat, lon, 50 * 1 / mapScale[0], 50 * 1 / mapScale[1]);
         }
     }
 }
